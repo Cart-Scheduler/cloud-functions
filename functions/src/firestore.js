37c4f464bf234bab2bd08ccfaf549d50075a682f
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2023-06-21 23:50:09 +0300

    serializableClone

--- functions/src/firestore.js
+++ functions/src/firestore.js
@@ -1,4 +1,5 @@
-const { getFirestore } = require('firebase-admin/firestore');
+const { getFirestore, Timestamp } = require('firebase-admin/firestore');
+const logger = require('firebase-functions/logger');
 
 const db = getFirestore();
 
@@ -29,10 +30,38 @@ exports.docExists = async (collection, id) => {
 
 /**
  * Returns true if given API key is valid.
+ * Valid API key is defined in Firestore: admin/functions.apiKey
  * @param {string} apiKey
  * @return {boolean} true if API key is valid
  */
 exports.isValidApiKey = async (apiKey) => {
   const doc = await readDoc('admin', 'functions');
+  if (doc === undefined) {
+    logger.warn('Please define API key in Firestore: admin/functions.apiKey');
+    return false;
+  }
   return apiKey === doc.apiKey;
 };
+
+/**
+ * Deep clones given objects but converts Firestore.Timestamp to
+ * primitive number.
+ * @param {object} obj - Object to be cloned
+ * @return {object} Cloned object
+ */
+const serializableClone = (obj) => {
+  const clone = {};
+  for (const prop in obj) {
+    if (Object.hasOwn(obj, prop)) {
+      let val = obj[prop];
+      if (val instanceof Timestamp) {
+        val = val.toMillis();
+      } else if (!Array.isArray(val) && val instanceof Object) {
+        val = serializableClone(val);
+      }
+      clone[prop] = val;
+    }
+  }
+  return clone;
+};
+exports.serializableClone = serializableClone;
